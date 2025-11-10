cd02348fdc8a2139381d6d7d1ff9b5ccc74f74c8
SonnyZ789123 <yoran.mertens@telenet.be>
2025-11-10 16:31:38 +0100

    Initial commit with R3F and styled-components installed

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Canvas } from '@react-three/fiber';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+const App = () => {
+  return (
+    <div id="canvas-container">
+      <GlobalStyle />
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} color="red" />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App;
